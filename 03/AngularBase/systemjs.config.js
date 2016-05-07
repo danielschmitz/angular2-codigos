d826cd9d5d41f6bdd6c67ad485b779b1bec373ae
Daniel Schmitz <danieljfa@gmail.com>
2016-05-07 10:10:52 -0300

    Alteração no AngularBase para o

---
+++ 03/AngularBase/systemjs.config.js
@@ -0,0 +1,45 @@
+(function (global) {
+
+    // mapa de carregamento do systemjs
+    var map = {
+        'app': 'app', // 'dist',
+        'rxjs': 'node_modules/rxjs',
+        'angular2-in-memory-web-api': 'node_modules/angular2-in-memory-web-api',
+        '@angular': 'node_modules/@angular'
+    };
+
+    // pacotes que o systemjs pode carregar
+    //  caso não encontre o arquivo no mapa
+    var packages = {
+        'app': { main: 'boot.js', defaultExtension: 'js' },
+        'rxjs': { defaultExtension: 'js' },
+        'angular2-in-memory-web-api': { defaultExtension: 'js' },
+    };
+
+    var packageNames = [
+        '@angular/common',
+        '@angular/compiler',
+        '@angular/core',
+        '@angular/http',
+        '@angular/platform-browser',
+        '@angular/platform-browser-dynamic',
+        '@angular/router',
+        '@angular/router-deprecated',
+        '@angular/testing',
+        '@angular/upgrade',
+    ];
+
+    // mapeia os pacotes do angular de acordo com o packageName acima
+    packageNames.forEach(function (pkgName) {
+        packages[pkgName] = { main: 'index.js', defaultExtension: 'js' };
+    });
+
+    var config = {
+        map: map,
+        packages: packages
+    }
+
+    if (global.filterSystemConfig) { global.filterSystemConfig(config); }
+    System.config(config);
+
+})(this);
